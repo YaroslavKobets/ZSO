6b4459c283d2d93957ff6c59d6ec2ecd1170f9a6
YaroslavKobets <63846184+YaroslavKobets@users.noreply.github.com>
2020-10-14 19:38:58 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,83 @@
+
+var myFullpage = new fullpage('#fullpage', {
+	//Navigation
+	menu: '#menu',
+	lockAnchors: true,
+	anchors:['intro', 'direction', 'video', 'ZSO', 'news', 'products'],
+	navigation: true,
+	navigationPosition: 'right',
+	navigationTooltips: ['О заводе', 'Направления', 'Видео о нас', 'ZSO в цифрах', 'Пресс-центр', 'Aссортимент'],
+	showActiveTooltip: true,
+	slidesNavigation: true,
+	slidesNavPosition: 'bottom',
+
+	//Scrolling
+	css3: true,
+	scrollingSpeed: 700,
+	autoScrolling: true,
+	fitToSection: false,
+	fitToSectionDelay: 1000,
+	scrollBar: false,
+	easing: 'easeInOutCubic',
+	easingcss3: 'ease',
+	loopBottom: false,
+	loopTop: false,
+	loopHorizontal: true,
+	continuousVertical: false,
+	continuousHorizontal: false,
+	scrollHorizontally: false,
+	interlockedSlides: true,
+	dragAndMove: false,
+	offsetSections: false,
+	resetSliders: false,
+	fadingEffect: false,
+	normalScrollElements: '#element1, .element2',
+	scrollOverflow: false,
+	scrollOverflowReset: false,
+	scrollOverflowOptions: null,
+	touchSensitivity: 15,
+	bigSectionsDestination: null,
+
+	//Accessibility
+	keyboardScrolling: true,
+	animateAnchor: true,
+	recordHistory: true,
+
+	//Design
+	controlArrows: true,
+	verticalCentered: true,
+	sectionsColor : ['#ccc', '#fff'],
+	paddingTop: '0em',
+	paddingBottom: '0px',
+	fixedElements: '#nav, #footer',
+	responsiveWidth: 0,
+	responsiveHeight: 0,
+	responsiveSlides: false,
+	parallax: false,
+	parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+	cards: false,
+	cardsOptions: {perspective: 100, fadeContent: true, fadeBackground: true},
+
+	//Custom selectors
+	sectionSelector: '.section',
+	slideSelector: '.sliders',
+
+	lazyLoading: true,
+
+	//events
+	onLeave: function(origin, destination, direction){},
+	afterLoad: function(origin, destination, direction){},
+	afterRender: function(){},
+	afterResize: function(width, height){},
+	afterReBuild: function(){},
+	afterResponsive: function(isResponsive){},
+	afterSlideLoad: function(section, origin, destination, direction){},
+	onSlideLeave: function(section, origin, destination, direction){}
+});
+$(function () {
+	$('[data-toggle="popover"]').popover({
+		trigger: 'focus'
+		
+	})
+});
+document.querySelector('').onwheel = e => e.stopPropagation();
